2e2276825031957834add9d9425b44fdfc17ab6b
demostanis <demostanis@protonmail.com>
2024-01-24 00:12:11 +0100

    filter out staff and test users

--- web/run.go
+++ web/run.go
@@ -14,7 +14,11 @@ const (
 func Run(db *gorm.DB) {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		var users []models.User
-		db.Limit(UsersPerPage).Order("level DESC").Find(&users)
+		db.
+			Limit(UsersPerPage).
+			Order("level DESC").
+			Where("is_staff = false AND is_test = false").
+			Find(&users)
 
 		index(users).Render(r.Context(), w)
 	})
