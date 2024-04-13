56a8ad1deb8b18ab902c15ea96b97126c0880d2d
demostanis <demostanis@protonmail.com>
2024-04-13 20:27:29 +0200

    replace custom made batch implementation with db.FindInBatches

--- web/peerfinder.go
+++ web/peerfinder.go
@@ -14,6 +14,15 @@ const (
 	usersPerQuery = 10000
 )
 
+func isValidProject(project models.Project) bool {
+	// we need this bunch of conditions since GORM will give us
+	// zeroed projects which don't meet the preload condition...
+	return len(project.Teams) > 0 &&
+		len(project.Teams) > project.ActiveTeam &&
+		len(project.Teams[project.ActiveTeam].Users) > 0 &&
+		project.Teams[project.ActiveTeam].Users[0].User.ID != 0
+}
+
 func handlePeerFinder(db *gorm.DB) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		var subjects []models.Subject
@@ -43,39 +52,25 @@ func handlePeerFinder(db *gorm.DB) http.Handler {
 			checkedSubjects[subject] = true
 		}
 
-		i := 0
-		var totalProjects []models.Project
-		for {
+		var projects []models.Project
+		db.
+			Where("status != 'finished'").
+			Preload("Teams.Users.User",
+				"campus_id = ? AND "+database.OnlyRealUsersCondition,
+				getLoggedInUser(r).them.CampusID).
+			Preload("Subject", "name IN ? AND "+database.UnwantedSubjectsCondition,
+				wantedSubjects).
+			Model(&models.Project{}).
 			// We need to fetch by batches of users, else GORM
 			// generates way too large queries...
-			campusId := getLoggedInUser(r).them.CampusID
-			var projects []models.Project
-			db.
-				Where("status != 'finished'").
-				Preload("Teams.Users.User",
-					"campus_id = ? AND "+database.OnlyRealUsersCondition,
-					campusId).
-				Preload("Subject", "name IN ? AND "+database.UnwantedSubjectsCondition,
-					wantedSubjects).
-				Limit(usersPerQuery).
-				Offset(usersPerQuery * i).
-				Model(&models.Project{}).
-				Find(&projects)
-			if len(projects) == 0 {
-				break
-			}
-			totalProjects = append(totalProjects, projects...)
-			i++
-		}
+			FindInBatches(&projects, usersPerQuery,
+				func(db *gorm.DB, batch int) error {
+					return nil
+				})
 
 		projectsMap := make(map[int][]models.Project)
-		for _, project := range totalProjects {
-			// we need this bunch of conditions since GORM will give us
-			// zeroed projects which don't meet the preload condition...
-			if len(project.Teams) > 0 &&
-				len(project.Teams) > project.ActiveTeam &&
-				len(project.Teams[project.ActiveTeam].Users) > 0 &&
-				project.Teams[project.ActiveTeam].Users[0].User.ID != 0 {
+		for _, project := range projects {
+			if isValidProject(project) {
 				projectsMap[project.Subject.ID] = append(
 					projectsMap[project.Subject.ID], project)
 			}
